418b7efb8a80a39551fde9fbbe0edd2ec12d7bbf
mingrammer <mingrammer@gmail.com>
2019-12-12 01:05:06 +0900

    Change text

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fib Calculator
           </a>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
